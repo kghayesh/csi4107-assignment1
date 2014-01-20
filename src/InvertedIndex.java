c625044120afcec5d13cd39f317fa450e8a63415
Khaled <kghayesh@gmail.com>
2014-01-20 10:38:17 -0500

    meeting 1

---
+++ src/InvertedIndex.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+
+public class InvertedIndex {
+	
+	public Hashtable<Token, ArrayList<DocIndex>> index;
+	
+	public void AddTweet(Tweet input)
+	{
+	
+	}
+
+}
---
+++ src/Preprocessing.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Preprocessing {
+
+	public String removeStopwords(String text)
+	{
+		
+	}
+	
+	public String removePunctuation(String text)
+	{
+		
+	}
+	
+	public ArrayList<String> tokenize(String text)
+	{
+		
+	}
+}
