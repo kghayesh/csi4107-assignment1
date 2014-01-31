c9ae6f74ed9cd4734daa5190b36118eac1bc207a
Brodie Gallinger <brodie.gallinger@gmail.com>
2014-01-31 16:10:57 -0500

    deleted the extra copy of the CLI code

--- src/DocIndex.java
+++
@@ -1,8 +0,0 @@
-// Stores the occurence of each token in each document and stores the number. To be used in the hashtable
-// of the inverted index
-
-public class DocIndex {
-
-	public String ID;
-	public int tokenFrequency;
-}
--- src/Token.java
+++
@@ -1,6 +0,0 @@
-
-public class Token {
-
-	public String text;
-	public int frequency;
-}
