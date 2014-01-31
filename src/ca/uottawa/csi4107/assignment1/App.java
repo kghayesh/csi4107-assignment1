6ffd2655210e2df71ee00e0bb88e4dd16048916b
Brodie Gallinger <brodie.gallinger@gmail.com>
2014-01-31 16:05:18 -0500

    Working CLI version

--- src/ca/uottawa/csi4107/assignment1/App.java
+++ src/ca/uottawa/csi4107/assignment1/App.java
@@ -5,9 +5,11 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.ListIterator;
 
+import org.apache.commons.cli.*;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
@@ -25,76 +27,96 @@ import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopScoreDocCollector;
 import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.store.SimpleFSDirectory;
 import org.apache.lucene.util.Version;
 
 public class App {
+	
+	Directory index;
+	StandardAnalyzer analyzer;
+	IndexSearcher searcher;
+	
+	public App()
+	{
+		analyzer = new StandardAnalyzer(Version.LUCENE_46);
+	}
+	
 
 	public static void main(String[] args) throws ParseException, IOException
 	{
 		App app = new App();
 		
-		String path = app.getClass().getClassLoader().getResource(".").getPath();
-		String fileName = "Trec_microblog11.txt";
-		path = "/Volumes/Media/dev/workspace/csi-4107-assignment1-tokenizer-test/res/Trec_microblog11.txt";
-		System.out.println("Path to tweets file: " + path);
+		app.initCLI(args);
+	}
+	
+	
+	private void initCLI(String[] args)
+	{
+		// CLI Options
+		Option index = new Option("index", true, "Index a set of tweets");
+		index.setArgName("tweets");
+		index.setArgs(1);
 		
+		Option querySet = new Option("queryset", true, "Perform a batch set of queries from a file");
+		index.setArgName("queryfile");
+		index.setArgs(1);
 		
-		// Convert the tweets into Documents
-		List<Document> tweets = null;
-		try {
-			tweets = app.parseTweets(path);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		Option query = new Option("query", true, "Make a query on the index of tweets");
+		index.setArgName("query");
+		index.setArgs(1);
+		
+		Option help = new Option("help", "Print usage information");
 	
 		
+		Options options = new Options();
+		
+		options.addOption(index);
+		options.addOption(querySet);
+		options.addOption(query);
+		options.addOption(help);
 		
 		
-		// Create the Analyzer, Directory, and IndexWriter
-		StandardAnalyzer analyzer = new StandardAnalyzer(Version.LUCENE_46);
-		// Initialize the index file
+		CommandLineParser parser = new BasicParser();
 		
-		// Path where index will be stored
-		String tweetIndexPath = "/Volumes/Media/dev/workspace/csi-4107-assignment1-tokenizer-test/res/tweet_index";
-		System.out.println("Path to index directoy: " + tweetIndexPath);
-		
-		File tweetIndex = new File(tweetIndexPath);
-		Directory index = new SimpleFSDirectory(tweetIndex);
-		
-		
-		// Index the parsed tweet Documents
-		app.indexTweets(tweets, analyzer, index);
-		
-		
-		
-		// Create a Query
-		String queryString = "BBC"; // Hard coded keyword query for now
-		System.out.println("Querying index for keywords: " +  queryString);
-		Query q = app.getQuery(queryString, analyzer);
-		
-		
-		
-		// Search
-		IndexReader reader = null;
 		try {
-			reader = DirectoryReader.open(index);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			CommandLine line = parser.parse(options, args);
+			
+			if(line.hasOption("index"))
+			{
+				indexTweets(line.getOptionValue("index"));
+			}
+			
+			if(line.hasOption("queryset"))
+			{
+				if(indexExists())
+				{
+					initIndex();
+					searchTweetsFromBatch(line.getOptionValue("queryset"));
+				}
+				else
+					System.out.println("Index has not been built");
+			}
+			
+			if(line.hasOption("query"))
+			{
+				if(indexExists())
+				{
+					initIndex();
+					searchTweets(line.getOptionValue("query"));
+				}
+				else
+					System.out.println("Index has not been built");
+			}
+			if(line.hasOption("help"))
+			{
+				HelpFormatter formatter = new HelpFormatter();
+				formatter.printHelp( "tweetSearch", options );
+			}
+		}
+		catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		int hitsPerPage = 30;
-		IndexSearcher searcher = new IndexSearcher(reader);
-		TopScoreDocCollector collector = TopScoreDocCollector.create(hitsPerPage, true);
-		searcher.search(q, collector);
-		
-		ScoreDoc[] hits = collector.topDocs().scoreDocs;
-		
-		
-		
-		app.displayResults(hits, searcher);
-		
 	}
 	
 	
@@ -110,24 +132,10 @@ public class App {
 	}
 	
 	
-	private Query getQuery(String queryString, Analyzer analyzer)
-	{
-		Query q = null;
-		
-		try {
-			q = new QueryParser(Version.LUCENE_46, "text", analyzer).parse(queryString);
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		return q;
-	}
-	
 	
 	private List<Document> parseTweets(String filePath) throws IOException
 	{
-		List<Document> docs = new ArrayList<Document>();
+		LinkedList<Document> docs = new LinkedList<Document>();
 		
 		FileReader input = null;
 		BufferedReader bufferedRead = null;
@@ -136,7 +144,6 @@ public class App {
 		try 
 		{
 			input = new FileReader(filePath);
-			
 			bufferedRead = new BufferedReader(input);
 				
 			while( (tweet = bufferedRead.readLine()) != null)
@@ -155,29 +162,112 @@ public class App {
 			if (bufferedRead != null)  { bufferedRead.close();  }
 		}
 		
-		
-		
 		return docs;
 	}
 	
 	
-	private Directory indexTweets(List<Document> tweets, Analyzer analyzer,Directory index) throws IOException 
+	private void indexTweets(String tweetsPath) throws IOException
 	{
-		// Configure and initialize the IndexWriter
-		IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_46, analyzer);
-		IndexWriter writer = null;
-			writer = new IndexWriter(index, config);
+		// Parse the tweets from the file into Documents objects
+		List<Document> tweets = parseTweets(tweetsPath);
 		
 		
-		// Go through the list of tweet Documents and add them to the index
-		ListIterator<Document> iterator = tweets.listIterator();
-		while (iterator.hasNext())
+		// Initialize the index file if it doesn't already exist then build the index. If id does exist, we don't rebuild the index
+		initIndex();
+		
+		if (!indexExists())
 		{
-			writer.addDocument(iterator.next());
+			// Configure and initialize the IndexWriter
+			IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_46, analyzer);
+			IndexWriter writer = new IndexWriter(index, config);
+					
+			
+			// Go through the list of tweet Documents and add them to the index
+			ListIterator<Document> iterator = tweets.listIterator();
+			while (iterator.hasNext())
+			{
+				writer.addDocument(iterator.next());
+			}
+			writer.close();
 		}
-		writer.close();
+	}
+	
+	private void initIndex()
+	{
 		
-		return index;
+		try {
+				index = FSDirectory.open(new File("res/index"));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	}
+	
+	private boolean indexExists()
+	{
+		return new File("res/index").exists();
+	}
+	
+	
+	private ScoreDoc[] searchTweets(Query q) throws IOException
+	{
+		int hitsPerPage = 1000;
+		searcher = new IndexSearcher(DirectoryReader.open(index));
+		TopScoreDocCollector collector = TopScoreDocCollector.create(hitsPerPage, true);
+		
+		searcher.search(q, collector);
+		
+		return collector.topDocs().scoreDocs;
+	}
+	
+	
+	private void searchTweets(String queryString) throws IOException
+	{
+		ScoreDoc[] hits = searchTweets(getQuery(queryString));
+		
+		displayResults(hits, searcher);
+	}
+	
+	
+	
+	private void searchTweetsFromBatch(String queryFilePath)
+	{
+		// Parse the queries text file and make a list of Query objects
+		
+		
+		// Make a search for each query and output the results
+		
+		
+		
+	}
+	
+	
+	private Query getQuery(String queryString)
+	{
+		Query q = null;
+		
+		try {
+			q = new QueryParser(Version.LUCENE_46, "text", analyzer).parse(queryString);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return q;
+	}
+	
+	
+	private void outputResults(ScoreDoc[] hits, IndexSearcher searcher)
+	{
+		// Make the Results file out of search results
+	}
+	
+	
+	// Displays a sample of indexed vocabulary. May or may not be possible programmatically 
+	private void displayIndexTerms()
+	{
+		
+		
 	}
 
 }
